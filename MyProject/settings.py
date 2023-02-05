829b5fb51ee7cd5fdc2036f07ef90c0dbd8ed2ba
JG-11 <80846488+JeeveshGarg@users.noreply.github.com>
2023-02-05 17:37:50 +0530

    version 0.0.1

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1%t5q*-!xasj(vypwrq0pihvz6y3rz%c@0$4@s0nopoy(5^li4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
