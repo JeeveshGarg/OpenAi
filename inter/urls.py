7e111c5009fd70a6a856e89b4ba4862cd669059c
JG-11 <80846488+JeeveshGarg@users.noreply.github.com>
2023-02-07 00:28:39 +0530

    debug3

--- inter/urls.py
+++ inter/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import SummaryCreateView
-from .views import SimilarityCreateView
+from .views import SummaryCreateView, SimilarityCreateView
 
 
 urlpatterns = [
